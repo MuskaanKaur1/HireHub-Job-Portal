0d06302b003e56978da31d58774295af3c52fef9
MuskaanKaur1 <kaurmuskaan172@gmail.com>
2026-04-23 20:23:50 +0530

    Update jobRoutes.js

--- server/routes/jobRoutes.js
+++ server/routes/jobRoutes.js
@@ -1,5 +1,5 @@
 import express from "express";
-import { createJob, getJobs, deleteJob } from "../controllers/jobController.js";
+import { createJob, getJobs, deleteJob, updateJob } from "../controllers/jobController.js";
 import { protect } from "../middleware/authMiddleware.js";
 import { adminOnly } from "../middleware/adminMiddleware.js";
 
@@ -9,5 +9,7 @@ const router = express.Router();
 router.post("/create", protect,adminOnly, createJob);
 router.get("/", getJobs);
 router.delete("/:id", protect, adminOnly, deleteJob);
+router.put("/:id", protect, adminOnly, updateJob);
+
 
 export default router; 
