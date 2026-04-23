54245a96a716e8585e06798a51f285adfdbdb9ad
MuskaanKaur1 <kaurmuskaan172@gmail.com>
2026-04-23 20:31:21 +0530

    Add files via upload

---
+++ client/src/components/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import { Children } from "react";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({children})=>{
+    const token = localStorage.getItem("token");
+
+    if (!token || token === "undefined"){
+        return <Navigate to="/login"/>;
+    }
+
+    return children;
+};
+
+export default ProtectedRoute;
